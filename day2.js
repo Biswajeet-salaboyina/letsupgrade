32b6004b373f498ec5ca6a63a4ae00ff3222a016
Biswajeet-salaboyina <60876660+Biswajeet-salaboyina@users.noreply.github.com>
2021-09-08 20:04:23 +0530

    day 2

---
+++ day2.js
@@ -0,0 +1,14 @@
+const k=[1,4,3,4,5,6,89,8,7,6];
+for (var i=0; i<k.length;i++)
+{
+    if(k[i]%2==0)
+    {
+        console.log(k[i]);
+    }
+}
+
+
+
+const a=["abc","ert","etg"];
+var p=a.reverse();
+console.log(p);
