ffc4be65d8ee4798e2d58f0b09610625d84b2a3d
Biswajeet-salaboyina <60876660+Biswajeet-salaboyina@users.noreply.github.com>
2021-09-09 19:43:27 +0530

    day 3 Todo

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+
+
+function todo() {
+    let data = document.getElementById('inp').value;
+    document.getElementById('two').innerHTML += `<h1 class='todo'>${data}</h1>`;
+    document.getElementById('inp').value = "";
+}
+
+function changeColor() {
+
+    document.getElementById('two').style.backgroundColor = "black";
+
+}
+
+
+
+
+
+
+
+
